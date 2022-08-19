536d574f18959e431addee227da7ebfa3fe167c2
Timon Van Poucke <timonvanpoucke@hotmail.com>
2022-08-19 03:12:35 +0200

    Add logging

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const { initializeData } = require('./data/index');
 const recipe = require('./rest/recipe');
 const user = require('./rest/user');
 const login = require('./rest/login');
+const emoji = require('node-emoji');
 
 const HOST = config.get('host');
 const PORT = config.get('port');
@@ -34,6 +35,32 @@ async function main() {
     );
     
     app.use(bodyParser());
+  app.use(async (ctx, next) => {
+    const logger = getLogger();
+    logger.info(`${emoji.get('fast_forward')} ${ctx.method} ${ctx.url}`);
+
+    const getStatusEmoji = () => {
+      if (ctx.status >= 500) return emoji.get('skull');
+      if (ctx.status >= 400) return emoji.get('x');
+      if (ctx.status >= 300) return emoji.get('rocket');
+      if (ctx.status >= 200) return emoji.get('white_check_mark');
+      return emoji.get('rewind');
+    };
+
+    try {
+      await next();
+
+      logger.info(
+        `${getStatusEmoji()} ${ctx.method} ${ctx.status} ${ctx.url}`,
+      );
+    } catch (error) {
+      logger.error(`${emoji.get('x')} ${ctx.method} ${ctx.status} ${ctx.url}`, {
+        error,
+      });
+
+      throw error;
+    }
+  });
     
     app
         .use(recipe.router.routes())
